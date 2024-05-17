bee62d18589fac159c8cc5e559e2410984cd4275
jordi <jordielgueta@gmail.com>
2024-05-17 22:06:15 +0200

    comitaso

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -2,7 +2,6 @@ import { google } from "googleapis";
 
 export default async function Page() {
   async function getReviews() {
-    "use server";
     const auth = await google.auth.getClient({
       credentials: {
         client_id: process.env.GOOGLE_CLIENT_ID,
@@ -11,22 +10,22 @@ export default async function Page() {
         private_key: process.env.GOOGLE_PRIVATE_KEY,
       },
       // keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-      scopes: ["https://www.googleapis.com/auth/cloud-platform"],
+      scopes: ["https://www.googleapis.com/auth/business.manage"],
     });
 
-    const places = google.places({
+    const mybusiness = google.mybusinessplaceactions({
       version: "v1",
       auth,
     });
 
     try {
-      const placeData = await places.places.get({
-        languageCode: "en",
+      const placeData = await mybusiness.locations.placeActionLinks.get({
         // name: `places/${process.env.MAPS_PLACE_ID}`,
         // Does this get reviews directly?
-        name: `places/${process.env.MAPS_PLACE_ID}?fields=reviews`,
+        // name: `places/${process.env.MAPS_PLACE_ID}?fields=reviews`,
+        name: `accounts/${process.env.GOOGLE_PROJECT_ID}/locations/${process.env.MAPS_PLACE_ID}/reviews`,
       });
-      return placeData.data.reviews;
+      return placeData;
     } catch (error) {
       console.error("Error fetching reviews data:", error);
       return [];
@@ -34,19 +33,22 @@ export default async function Page() {
   }
 
   const reviews = await getReviews();
+  console.log(reviews);
 
   return (
     <div>
-      {reviews &&
+      {" "}
+      Hello
+      {/* {reviews  &&
         reviews.map((review, index) => (
           <div key={index}>
-            <strong>{review.name}</strong>: {review.rating},{" "}
-            {review.relativePublishTimeDescription}
+            <strong>{review.authorAttribution?.displayName}</strong>:{" "}
+            {review.rating}, {review.relativePublishTimeDescription}
             <p>{review.text?.text}</p>
             <strong>Original Text</strong>
             <p>{review.originalText?.text}</p>
           </div>
-        ))}
+        ))} */}
     </div>
   );
 }
