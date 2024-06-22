18bccee6a243cbd9684db391ccda3a62dfb20bf0
jordi <jordielgueta@gmail.com>
2024-06-22 12:08:31 +0200

    review front

--- src/app/reviews/_components/review-card.tsx
+++ src/app/reviews/_components/review-card.tsx
@@ -1,13 +1,4 @@
-"use client";
-
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardFooter,
-} from "@/components/ui/card";
-import StarRating from "@/components/star-rating"; // Import the StarRating component
 
 interface Review {
   name: string | null | undefined;
@@ -18,18 +9,83 @@ interface Review {
 
 const ReviewCard: React.FC<{ review: Review }> = ({ review }) => {
   return (
-    <Card className="border border-gray-300 rounded-lg p-4 mb-4 shadow-md w-[18rem]">
-      <CardHeader className="mb-2">
-        <h3 className="text-lg font-semibold">{review.name}</h3>
-        <div className="flex items-center text-gray-600">
-          {review.rating ? <StarRating rating={review.rating} /> : "No rating"}
-          <span className="ml-2">{review.relativePublishTimeDescription}</span>
+    <article className="md:gap-8 sm:grid sm:grid-cols-3 2xl:ml-20 2xl:mr-80">
+      <div className="col-span-1 mt-6 md:mt-0 mr-16 flex flex-col items-center justify-start">
+        <div className=" mb-6 w-full">
+          <div className="font-medium">
+            <p>Jese Leos</p>
+            <div className="text-sm text-gray-500">
+              <div className="flex items-center mb-1">
+                {[...Array(5)].map((_, i) => (
+                  <svg
+                    key={i}
+                    aria-hidden="true"
+                    className="w-5 h-5 text-yellow-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>{`${i + 1} star`}</title>
+                    <path d="M9.049 2.927a1 1 0 011.902 0l1.135 3.495h3.692a1 1 0 01.594 1.81l-2.986 2.173 1.135 3.495a1 1 0 01-1.537 1.11L10 12.347l-2.986 2.173a1 1 0 01-1.537-1.11l1.135-3.495-2.986-2.173a1 1 0 01.594-1.81h3.692L9.049 2.927z" />
+                  </svg>
+                ))}
+                <h3 className="ml-2 text-sm font-semibold text-gray-90">5/5</h3>
+              </div>
+            </div>
+            <div>
+              <dl>
+                <dt className="text-sm font-medium text-gray-500">Service</dt>
+                <dd className="flex items-center mb-3">
+                  <div className="w-full bg-gray-200 rounded h-2.5 me-2">
+                    <div className="bg-blue-600 h-2.5 rounded dark:bg-blue-500 w-[88%]"></div>
+                  </div>
+                  <span className="text-sm font-medium text-gray-500">8.8</span>
+                </dd>
+              </dl>
+              <dl>
+                <dt className="text-sm font-medium text-gray-500">Room</dt>
+                <dd className="flex items-center mb-3">
+                  <div className="w-full bg-gray-200 rounded h-2.5 me-2">
+                    <div className="bg-blue-600 h-2.5 rounded dark:bg-blue-500 w-[89%]"></div>
+                  </div>
+                  <span className="text-sm font-medium text-gray-500">8.9</span>
+                </dd>
+              </dl>
+
+              <dl>
+                <dt className="text-sm font-medium text-gray-500">Location</dt>
+                <dd className="flex items-center">
+                  <div className="w-full bg-gray-200 rounded h-2.5 me-2">
+                    <div className="bg-blue-600 h-2.5 rounded dark:bg-blue-500 w-[54%]"></div>
+                  </div>
+                  <span className="text-sm font-medium text-gray-500">5.4</span>
+                </dd>
+              </dl>
+            </div>
+          </div>
         </div>
-      </CardHeader>
-      <CardContent className="mb-2">
-        <p className="text-black">{review.text}</p>
-      </CardContent>
-    </Card>
+      </div>
+      <div className="col-span-2 mt-6 md:mt-0">
+        <div className="flex items-start mb-5">
+          <div className="pe-4">
+            <footer>
+              <p className="mb-2 text-sm text-gray-500">
+                Reviewed: <time>January, 2022</time>
+              </p>
+            </footer>
+          </div>
+        </div>
+        <p className="mb-2 text-gray-500">
+          This is my third Invicta Pro Diver. They are just fantastic value for
+          money. This one arrived yesterday and the first thing I did was set
+          the time, popped on an exhibition case back, and observed the tick. It
+          was already running smoothly and kept time perfectly during the day.
+          The size is perfect, and the build quality and finish are exceptional
+          for the price. I hope it will continue to perform as well as it has
+          started.
+        </p>
+      </div>
+    </article>
   );
 };
 
--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,4 +1,5 @@
 import ReviewCard from "./_components/review-card";
+import { google } from "googleapis";
 
 interface Review {
   name: string | null | undefined;
@@ -8,168 +9,182 @@ interface Review {
   relativePublishTimeDescription: string | null | undefined;
 }
 
-async function getReviews() {
-  const res = await fetch("/api/reviews");
+export default async function Page() {
+  async function getReviews() {
+    "use server";
+    const auth = await google.auth.getClient({
+      credentials: {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+      },
+      // keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
 
-  if (!res.ok) {
-    // This will activate the closest `error.ts` Error Boundary
-    throw new Error("Failed to fetch data");
+    const sheets = google.sheets({
+      version: "v4",
+      auth,
+    });
+
+    try {
+      const response = await sheets.spreadsheets.values.get({
+        spreadsheetId: process.env.GOOGLE_SHEET_ID,
+        range: "Sheet1!A21:G",
+      });
+
+      return response.data.values;
+    } catch (error) {
+      console.error("Error getching sheets data:", error);
+      return [];
+    }
   }
 
-  return res.json();
-}
-
-export default async function Page() {
+  // const spots = await getReviews();
   // const reviews = await getReviews();
-  const reviews: Review[] = [
-    {
-      name: "John Doe",
-      rating: 4.5,
-      text: "This is a great product!",
-      originalText: "Ceci est un excellent produit!",
-      relativePublishTimeDescription: "1 week ago",
-    },
-    {
-      name: "Jane Smith",
-      rating: 5,
-      text: "Absolutely loved it!",
-      originalText: "Absolutamente me encantó!",
-      relativePublishTimeDescription: "2 weeks ago",
-    },
-    // Add more reviews as needed
-  ];
 
   return (
     <div>
-      <div className="flex items-center mb-2">
-        <svg
-          className="w-4 h-4 text-yellow-400 me-1"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 22 20"
-        >
-          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-        </svg>
-        <svg
-          className="w-4 h-4 text-yellow-400 me-1"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 22 20"
-        >
-          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-        </svg>
-        <svg
-          className="w-4 h-4 text-yellow-400 me-1"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 22 20"
-        >
-          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-        </svg>
-        <svg
-          className="w-4 h-4 text-yellow-400 me-1"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 22 20"
-        >
-          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-        </svg>
-        <svg
-          className="w-4 h-4 text-gray-300 me-1 dark:text-gray-500"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 22 20"
-        >
-          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-        </svg>
-        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
-          4.95
+      <div className="mb-20">
+        <div className="flex items-center mb-2">
+          <svg
+            className="w-4 h-4 text-yellow-400 me-1"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <svg
+            className="w-4 h-4 text-yellow-400 me-1"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <svg
+            className="w-4 h-4 text-yellow-400 me-1"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <svg
+            className="w-4 h-4 text-yellow-400 me-1"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <svg
+            className="w-4 h-4 text-gray-300 me-1 dark:text-gray-500"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+            4.95
+          </p>
+          <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+            out of
+          </p>
+          <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+            5
+          </p>
+        </div>
+        <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+          1,745 global ratings
         </p>
-        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
-          out of
-        </p>
-        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
-          5
-        </p>
-      </div>
-      <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-        1,745 global ratings
-      </p>
-      <div className="flex items-center mt-4">
-        <a
-          href="#"
-          className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
-        >
-          5 star
-        </a>
-        <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-          <div className="h-5 bg-yellow-400 rounded width-[70%]"></div>
+        <div className="flex items-center mt-4">
+          <a
+            href="#"
+            className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          >
+            5 star
+          </a>
+          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+            <div className="h-5 bg-yellow-400 rounded width-[70%]"></div>
+          </div>
+          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+            70%
+          </span>
         </div>
-        <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-          70%
-        </span>
-      </div>
-      <div className="flex items-center mt-4">
-        <a
-          href="#"
-          className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
-        >
-          4 star
-        </a>
-        <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-          <div className="h-5 bg-yellow-400 rounded w-[17%]"></div>
+        <div className="flex items-center mt-4">
+          <a
+            href="#"
+            className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          >
+            4 star
+          </a>
+          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+            <div className="h-5 bg-yellow-400 rounded w-[17%]"></div>
+          </div>
+          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+            17%
+          </span>
         </div>
-        <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-          17%
-        </span>
-      </div>
-      <div className="flex items-center mt-4">
-        <a
-          href="#"
-          className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
-        >
-          3 star
-        </a>
-        <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-          <div className="h-5 bg-yellow-400 rounded w-[8%]"></div>
+        <div className="flex items-center mt-4">
+          <a
+            href="#"
+            className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          >
+            3 star
+          </a>
+          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+            <div className="h-5 bg-yellow-400 rounded w-[8%]"></div>
+          </div>
+          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+            8%
+          </span>
         </div>
-        <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-          8%
-        </span>
-      </div>
-      <div className="flex items-center mt-4">
-        <a
-          href="#"
-          className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
-        >
-          2 star
-        </a>
-        <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-          <div className="h-5 bg-yellow-400 rounded w-[4%]"></div>
+        <div className="flex items-center mt-4">
+          <a
+            href="#"
+            className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          >
+            2 star
+          </a>
+          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+            <div className="h-5 bg-yellow-400 rounded w-[4%]"></div>
+          </div>
+          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+            4%
+          </span>
         </div>
-        <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-          4%
-        </span>
-      </div>
-      <div className="flex items-center mt-4">
-        <a
-          href="#"
-          className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
-        >
-          1 star
-        </a>
-        <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-          <div className="h-5 bg-yellow-400 rounded w-[1%]"></div>
+        <div className="flex items-center mt-4">
+          <a
+            href="#"
+            className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          >
+            1 star
+          </a>
+          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+            <div className="h-5 bg-yellow-400 rounded w-[1%]"></div>
+          </div>
+          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+            1%
+          </span>
         </div>
-        <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-          1%
-        </span>
       </div>
+      <ReviewCard
+        review={{
+          name: null,
+          rating: null,
+          text: null,
+          relativePublishTimeDescription: null,
+        }}
+      />
     </div>
     /*     <div>
       {reviews ? (
