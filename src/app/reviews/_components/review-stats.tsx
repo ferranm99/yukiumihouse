7e3dca51d19be54e691a270be304edb18b4ae30f
jordi <jordielgueta@gmail.com>
2024-06-30 23:59:22 +0200

    commitaso

---
+++ src/app/reviews/_components/review-stats.tsx
@@ -0,0 +1,173 @@
+type Review = [
+  string, // name
+  string, // date
+  number, // rating
+  number | null, // serviceRating
+  number | null, // roomRating
+  number | null, // locationRating
+  string | null // reviewText
+];
+
+interface ReviewStatsProps {
+  reviews: Review[];
+}
+
+export default function ReviewStats({ reviews }: ReviewStatsProps) {
+  // Initialize an array with 5 elements, all set to 0
+  const starCounts = Array(5).fill(0);
+
+  reviews.forEach((review) => {
+    starCounts[review[2] - 1] += 1; // Subtract 1 to match index (0-4 for 1-5 stars)
+  });
+
+  const totalReviews = reviews.length;
+  const fiveStarPercentage = (starCounts[4] / totalReviews) * 100;
+  const fourStarPercentage = (starCounts[3] / totalReviews) * 100;
+  const threeStarPercentage = (starCounts[2] / totalReviews) * 100;
+  const twoStarPercentage = (starCounts[1] / totalReviews) * 100;
+  const oneStarPercentage = (starCounts[0] / totalReviews) * 100;
+
+  return (
+    <div className="mb-20 pl-16 mt-20">
+      <div className="flex items-center mb-2">
+        <svg
+          className="w-4 h-4 text-yellow-400 me-1"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          className="w-4 h-4 text-yellow-400 me-1"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          className="w-4 h-4 text-yellow-400 me-1"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          className="w-4 h-4 text-yellow-400 me-1"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg
+          className="w-4 h-4 text-gray-300 me-1"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+          {reviews.reduce((acc, review) => acc + review[2], 0) / reviews.length}
+        </p>
+        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+          out of
+        </p>
+        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+          5
+        </p>
+      </div>
+      <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+        {reviews.length} global ratings
+      </p>
+      <div className="flex items-center mt-4">
+        <a
+          href="#"
+          className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          5 star
+        </a>
+        <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+          <div
+            className={`h-5 bg-yellow-400 rounded w-[${fiveStarPercentage}%]`}
+          />
+        </div>
+        <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+          {fiveStarPercentage}%
+        </span>
+      </div>
+      <div className="flex items-center mt-4">
+        <a
+          href="#"
+          className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          4 star
+        </a>
+        <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+          <div
+            className={`h-5 bg-yellow-400 rounded w-[${fourStarPercentage}\%\]`}
+          />
+        </div>
+        <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+          {fourStarPercentage}%
+        </span>
+      </div>
+      <div className="flex items-center mt-4">
+        <a
+          href="#"
+          className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          3 star
+        </a>
+        <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+          <div
+            className={`h-5 bg-yellow-400 rounded w-[${threeStarPercentage}%]`}
+          />
+        </div>
+        <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+          {threeStarPercentage}%
+        </span>
+      </div>
+      <div className="flex items-center mt-4">
+        <a
+          href="#"
+          className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          2 star
+        </a>
+        <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+          <div
+            className={`h-5 bg-yellow-400 rounded w-[${twoStarPercentage}%]`}
+          />
+        </div>
+        <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+          {twoStarPercentage}%
+        </span>
+      </div>
+      <div className="flex items-center mt-4">
+        <a
+          href="#"
+          className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          1 star
+        </a>
+        <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+          <div
+            className={`h-5 bg-yellow-400 rounded w-[${oneStarPercentage}%]`}
+          />
+        </div>
+        <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+          {oneStarPercentage}%
+        </span>
+      </div>
+    </div>
+  );
+}
--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,10 +1,11 @@
 import ReviewCard from "./_components/review-card";
 import { google } from "googleapis";
+import ReviewStats from "./_components/review-stats";
 
 type Review = [
   string, // name
   string, // date
-  number, // rating (as string)
+  number, // rating
   number | null, // serviceRating
   number | null, // roomRating
   number | null, // locationRating
@@ -70,181 +71,9 @@ export default async function Page() {
 
   const reviews: Review[] = await getReviews();
 
-  const fiveStarPercentage =
-    (reviews.reduce((acc, review) => {
-      return review[2] === 5 ? acc + 1 : acc;
-    }, 0) /
-      reviews.length) *
-    100;
-  const fourStarPercentage =
-    (reviews.reduce((acc, review) => {
-      return review[2] === 4 ? acc + 1 : acc;
-    }, 0) /
-      reviews.length) *
-    100;
-  const threeStarPercentage =
-    (reviews.reduce((acc, review) => {
-      return review[2] === 3 ? acc + 1 : acc;
-    }, 0) /
-      reviews.length) *
-    100;
-  const twoStarPercentage =
-    (reviews.reduce((acc, review) => {
-      return review[2] === 2 ? acc + 1 : acc;
-    }, 0) /
-      reviews.length) *
-    100;
-  const oneStarPercentage =
-    (reviews.reduce((acc, review) => {
-      return review[2] === 1 ? acc + 1 : acc;
-    }, 0) /
-      reviews.length) *
-    100;
-
   return (
     <div>
-      <div className="mb-20 pl-16 mt-20">
-        <div className="flex items-center mb-2">
-          <svg
-            className="w-4 h-4 text-yellow-400 me-1"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 22 20"
-          >
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-          </svg>
-          <svg
-            className="w-4 h-4 text-yellow-400 me-1"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 22 20"
-          >
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-          </svg>
-          <svg
-            className="w-4 h-4 text-yellow-400 me-1"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 22 20"
-          >
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-          </svg>
-          <svg
-            className="w-4 h-4 text-yellow-400 me-1"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 22 20"
-          >
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-          </svg>
-          <svg
-            className="w-4 h-4 text-gray-300 me-1"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 22 20"
-          >
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-          </svg>
-          <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
-            {reviews.reduce((acc, review) => acc + review[2], 0) /
-              reviews.length}
-          </p>
-          <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
-            out of
-          </p>
-          <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
-            5
-          </p>
-        </div>
-        <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-          {reviews.length} global ratings
-        </p>
-        <div className="flex items-center mt-4">
-          <a
-            href="#"
-            className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
-          >
-            5 star
-          </a>
-          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-            <div
-              className={`h-5 bg-yellow-400 rounded w-[${fiveStarPercentage}%]`}
-            />
-          </div>
-          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-            {fiveStarPercentage}%
-          </span>
-        </div>
-        <div className="flex items-center mt-4">
-          <a
-            href="#"
-            className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
-          >
-            4 star
-          </a>
-          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-            <div
-              className={`h-5 bg-yellow-400 rounded w-[${fourStarPercentage}%]`}
-            />
-          </div>
-          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-            {fourStarPercentage}%
-          </span>
-        </div>
-        <div className="flex items-center mt-4">
-          <a
-            href="#"
-            className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
-          >
-            3 star
-          </a>
-          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-            <div
-              className={`h-5 bg-yellow-400 rounded w-[${threeStarPercentage}%]`}
-            />
-          </div>
-          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-            {threeStarPercentage}%
-          </span>
-        </div>
-        <div className="flex items-center mt-4">
-          <a
-            href="#"
-            className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
-          >
-            2 star
-          </a>
-          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-            <div
-              className={`h-5 bg-yellow-400 rounded w-[${twoStarPercentage}%]`}
-            />
-          </div>
-          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-            {twoStarPercentage}%
-          </span>
-        </div>
-        <div className="flex items-center mt-4">
-          <a
-            href="#"
-            className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
-          >
-            1 star
-          </a>
-          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-            <div
-              className={`h-5 bg-yellow-400 rounded w-[${oneStarPercentage}%]`}
-            />
-          </div>
-          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-            {oneStarPercentage}%
-          </span>
-        </div>
-      </div>
+      <ReviewStats reviews={reviews} />
       <div className="px-16 w-full">
         <div className="grid grid-cols-1 2xl:grid-cols-2 gap-8 w-full">
           {reviews.map((review, index) => (
