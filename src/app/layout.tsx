88757909b04395369b4a599278443436df98c0dc
ferranm99 <ferranm99@gmail.com>
2024-08-24 20:54:35 +0200

    fix gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/navbar";
 import MobileNav from "@/components/mobile-nav";
 import ReactQueryProvider from "@/app/react-query-provider";
 import Footer from "@/components/footer";
-import Head from "next/head";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -28,23 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        {/* Google Tag Manager */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16681422523"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-16681422523');
-            `,
-          }}
-        />
-      </Head>
+    <GoogleTagManager gtmId="AW-16681422523" />
       <ReactQueryProvider>
         <body
           className={cn(
